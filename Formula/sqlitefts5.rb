ee2e495ea0eb615235c282637781a313d422b23f
Justin Clift <justin@postgresql.org>
2019-02-16 01:33:05 +1100

    Update Mojave bottle sha256

--- Formula/sqlitefts5.rb
+++ Formula/sqlitefts5.rb
@@ -11,7 +11,7 @@ class Sqlitefts5 < Formula
     cellar :any
     #sha256 "eaef16904d5b1ce29dee315b04ff8739dc35f6e2268684eb9add4609e7226042" => :sierra
     #sha256 "eaef16904d5b1ce29dee315b04ff8739dc35f6e2268684eb9add4609e7226042" => :high_sierra
-    #sha256 "da0236652ddbe3d4f3c6311de1e5910ccdce75a16559425d71ccd3fb48c0189c" => :mojave
+    sha256 "6d0b15dcb76896ee7403c989496e553a1cc7b90f934233b0950ce6187397015e" => :mojave
   end
 
   def install
