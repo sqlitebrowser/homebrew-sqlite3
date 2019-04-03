00a5f74da2e3c2d1dff8b5c8871b011d25693b1f
Justin Clift <justin@postgresql.org>
2019-04-03 18:46:59 +1100

    Updating SQLCipher to v4.1.0, add RTREE and GEOPOLY extensions

--- Formula/sqlcipherdb4s.rb
+++ Formula/sqlcipherdb4s.rb
@@ -1,16 +1,15 @@
 class Sqlcipherdb4s < Formula
   desc "SQLite extension providing 256-bit AES encryption"
   homepage "https://www.zetetic.net/sqlcipher/"
-  url "https://github.com/sqlcipher/sqlcipher/archive/v4.0.1.tar.gz"
-  sha256 "2f803017378c7479cb791be59b7bad8392a15acddbcc094e4433581fe421f4ca"
+  url "https://github.com/sqlcipher/sqlcipher/archive/v4.1.0.tar.gz"
+  sha256 "65144ca3ba4c0f9cd4bae8c20bb42f2b84424bf29d1ebcf04c44a728903b1faa"
   head "https://github.com/sqlcipher/sqlcipher.git"
 
   bottle do
     root_url "https://nightlies.sqlitebrowser.org/homebrew_bottles"
     cellar :any
-    sha256 "b017765022c359fd2412c9b8ed113ce58d75e50e6d1d3b3fab2e25daccd46a66" => :mojave
-    sha256 "f3324b872772528fade9fa1b9daf6ae72547789fe3c091188c81c73f3987f5b2" => :high_sierra
-    #sha256 "8fcdf53d01a09aeec89caecaf3e4dd6fbc4b8c91058db7eb29d328788a286b3f" => :sierra
+    #sha256 "b017765022c359fd2412c9b8ed113ce58d75e50e6d1d3b3fab2e25daccd46a66" => :mojave
+    #sha256 "f3324b872772528fade9fa1b9daf6ae72547789fe3c091188c81c73f3987f5b2" => :high_sierra
   end
 
   depends_on "openssl"
@@ -25,7 +24,7 @@ class Sqlcipherdb4s < Formula
     ]
 
     # Build with full-text search enabled
-    args << "CFLAGS=-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_STAT4 -DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_FTS5"
+    args << "CFLAGS=-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_STAT4 -DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_FTS5 -DSQLITE_ENABLE_GEOPOLY -DSQLITE_ENABLE_RTREE"
 
     system "./configure", *args
     system "make"
