467fb487c4a6b2d63929f0e3e21f755c42ebcca9
Justin Clift <justin@postgresql.org>
2020-10-17 20:30:37 +1100

    Updated SQLite bottle for Mojave

--- Formula/sqlitefts5.rb
+++ Formula/sqlitefts5.rb
@@ -5,11 +5,11 @@ class Sqlitefts5 < Formula
   version "3.33.0"
   sha256 "106a2c48c7f75a298a7557bcc0d5f4f454e5b43811cc738b7ca294d6956bbb15"
 
-#  bottle do
-#    root_url "https://nightlies.sqlitebrowser.org/homebrew_bottles"
-#    cellar :any
-#    sha256 "efad1852dc33844d284ad97340b89115548b0c1c2bc0b18fb93c866d79ce7fb7" => :mojave
-#  end
+  bottle do
+    root_url "https://nightlies.sqlitebrowser.org/homebrew_bottles"
+    cellar :any
+    sha256 "897ce57abea6d9da953c11554ba577f2334237aee853f6f416e5efcb550b512e" => :mojave
+  end
 
   def install
     ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_COLUMN_METADATA=1"
